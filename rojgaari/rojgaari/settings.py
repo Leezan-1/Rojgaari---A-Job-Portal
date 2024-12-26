01406064cb78a0f3d9f8b8d0293f13d10ae7a8c0
Leezan <94422693+Leezan-1@users.noreply.github.com>
2024-12-26 11:58:50 +0545

    setting up the project for development

--- rojgaari/rojgaari/settings.py
+++ rojgaari/rojgaari/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jobboard',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / "templates",
+            BASE_DIR / 'templates',
+            BASE_DIR / 'jobboard/templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -118,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
